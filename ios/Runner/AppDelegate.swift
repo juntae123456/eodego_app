a3673922135ed6fbf412adadf4a563292f588882
indextrown <lghan00020@nate.com>
2024-11-25 22:52:48 +0900

    feat: ios 가능 업데이트

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,13 +2,19 @@ import UIKit
 import Flutter
 import GoogleMaps
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey(ProcessInfo.processInfo.environment["GOOGLE_MAPS_API_KEY"]!)
+    // API 키를 String으로 안전하게 변환
+    if let apiKey = Bundle.main.infoDictionary?["GOOGLE_MAP_API_KEY"] as? String {
+      GMSServices.provideAPIKey(apiKey)
+    } else {
+      fatalError("Google Maps API key is missing in Info.plist")
+    }
+    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
